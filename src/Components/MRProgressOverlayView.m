c101cf62adff49969027ca82566862271d65f2c7
Tuong Nguyen Manh <tuong@intelligentmobiles.com>
2023-10-02 09:54:11 +0200

    Fixed compile error with Xcode 15

--- src/Components/MRProgressOverlayView.m
+++ src/Components/MRProgressOverlayView.m
@@ -808,7 +808,7 @@ static void *MRProgressOverlayViewObservationContext = &MRProgressOverlayViewObs
                       NSStringFromSelector(@selector(setProgress:animated:)));
             #endif
         }
-        [((id)self.modeView) setProgress:self.progress];
+        [((MRProgressView*)self.modeView) setProgress:self.progress];
     } else {
         NSAssert(self.mode == MRProgressOverlayViewModeDeterminateCircular
                  || self.mode == MRProgressOverlayViewModeDeterminateHorizontalBar,
